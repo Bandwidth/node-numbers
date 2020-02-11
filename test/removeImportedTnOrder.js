286dac54cb32b01071babf06410502fe243637a5
Dan Tolbert <dtolbert@bandwidth.com>
2020-02-11 13:35:06 -0500

    removeImportedTnOrder tests & code

--- test/removeImportedTnOrder.js
+++ test/removeImportedTnOrder.js
@@ -1,9 +1,9 @@
 var lib = require("../");
 var helper = require("./helper");
 var nock = require("nock");
-var Disconnect = lib.Disconnect;
+var RemoveImportedTnOrder = lib.RemoveImportedTnOrder;
 
-describe("Disconnect", function(){
+describe("RemoveImportedTnOrder", function(){
   before(function(){
     nock.disableNetConnect();
     helper.setupGlobalOptions();
@@ -14,131 +14,86 @@ describe("Disconnect", function(){
   });
   describe("#list", function(){
     it("should return list of orders", function(done){
-      helper.nock().get("/accounts/FakeAccountId/disconnects").reply(200, helper.xml.order, {"Content-Type": "application/xml"});
+      helper.nock().get("/accounts/FakeAccountId/removeImportedTnOrders").reply(200, helper.xml.removeImportedTnOrderList, {"Content-Type": "application/xml"});
       var client = helper.createClient();
-      Disconnect.list(client, {}, done);
+      RemoveImportedTnOrder.list(client, {}, (err, items) => {
+        if(err){
+          return done(err);
+        }
+        items.totalCount.should.eql(2);
+        items.removeImportedTnOrderSummary.length.should.eql(2);
+        done();
+      });
     });
     it("should return list of orders (with default client)", function(done){
-      helper.nock().get("/accounts/FakeAccountId/disconnects").reply(200, helper.xml.order, {"Content-Type": "application/xml"});
-      Disconnect.list({}, done);
-    });
-  });
-  describe("#getNotes", function(){
-    it("should return notes", function(done){
-      helper.nock().get("/accounts/FakeAccountId/disconnects/1/notes").reply(200, helper.xml.notes, {"Content-Type": "application/xml"});
-      var order = new Disconnect();
-      order.id = 1;
-      order.client = helper.createClient();
-      order.getNotes(function(err, notes){
+      helper.nock().get("/accounts/FakeAccountId/removeImportedTnOrders").reply(200, helper.xml.removeImportedTnOrderList, {"Content-Type": "application/xml"});
+      RemoveImportedTnOrder.list({}, (err, items) => {
         if(err){
           return done(err);
         }
-        notes.length.should.equal(2);
-        notes[0].id.should.equal(11299);
-        notes[0].userId.should.equal("customer");
-        notes[0].description.should.equal("Test");
+        items.totalCount.should.eql(2);
+        items.removeImportedTnOrderSummary.length.should.eql(2);
         done();
-      });
-    });
-    it("should fail for error status code", function(done){
-      helper.nock().get("/accounts/FakeAccountId/disconnects/1/notes").reply(400);
-      var order = new Disconnect();
-      order.id = 1;
-      order.client = helper.createClient();
-      order.getNotes(function(err, notes){
-        if(err){
-          return done();
-        }
-        done(new Error("An error is estimated"));
-      });
     });
   });
-  describe("#addNote", function(){
-    it("should add new note", function(done){
-      var data = {userId: "customer", description: "Test"};
-      helper.nock().post("/accounts/FakeAccountId/disconnects/1/notes", helper.buildXml({note: data})).reply(200, "", {"Location": "/accounts/FakeAccountId/portins/1/notes/11299"});
-      helper.nock().get("/accounts/FakeAccountId/disconnects/1/notes").reply(200, helper.xml.notes, {"Content-Type": "application/xml"});
-      var order = new Disconnect();
+  describe("#getHistory", () => {
+    it("should fetch history of a removeImportedTnOrder", (done) => {
+      helper.nock().get("/accounts/FakeAccountId/removeImportedTnOrders/1/history").reply(200, helper.xml.removeImportedTnOrderHistory, {"Content-Type": "application/xml"});
+      var order = new RemoveImportedTnOrder();
       order.id = 1;
       order.client = helper.createClient();
-      order.addNote(data, function(err, note){
+      order.getHistory(function(err, history){
         if(err){
           return done(err);
         }
-        note.id.should.equal(11299);
-        note.userId.should.equal("customer");
-        note.description.should.equal("Test");
+        history.length.should.eql(4);
         done();
-      });
-    });
-    it("should fail on loading notes error", function(done){
-      var data = {userId: "customer", description: "Test"};
-      helper.nock().post("/accounts/FakeAccountId/disconnects/1/notes", helper.buildXml({note: data})).reply(200, "", {"Location": "/accounts/FakeAccountId/portins/1/notes/11299"});
-      helper.nock().get("/accounts/FakeAccountId/disconnects/1/notes").reply(500);
-      var order = new Disconnect();
-      order.id = 1;
-      order.client = helper.createClient();
-      order.addNote(data, function(err, note){
-        if(err){
-          return done();
-        }
-        done(new Error("An error is estimated"));
-      });
-    });
-    it("should fail if location header is missing", function(done){
-      var data = {userId: "customer", description: "Test"};
-      helper.nock().post("/accounts/FakeAccountId/disconnects/1/notes", helper.buildXml({note: data})).reply(400);
-      var order = new Disconnect();
-      order.id = 1;
-      order.client = helper.createClient();
-      debugger;
-      order.addNote(data, function(err, note){
-        if(err){
-          return done();
-        }
-        done(new Error("An error is estimated"));
-      });
-    });
-    it("should fail on error status code", function(done){
-      var data = {userId: "customer", description: "Test"};
-      helper.nock().post("/accounts/FakeAccountId/disconnects/1/notes", helper.buildXml({note: data})).reply(400);
-      var order = new Disconnect();
-      order.id = 1;
-      order.client = helper.createClient();
-      order.addNote(data, function(err, note){
-        if(err){
-          return done();
-        }
-        done(new Error("An error is estimated"));
       });
     });
   });
   describe("#disconnectNumbers", function(){
     it("should disconnect numbers", function(done){
+      var numbers = ["9199918388", "4158714245", "4352154439", "4352154466"]
       var data = {
-        disconnectTelephoneNumberOrder: {
-          name: "test",
-          _nameXmlElement: "name",
-         disconnectTelephoneNumberOrderType: {
-           telephoneNumberList: [{telephoneNumber: "111"}, {telephoneNumber: "222"}]
-         }
+        removeImportedTnOrder:{
+          customerOrderId: "test",
+          _nameXmlElement: "CustomerOrderId",
+          telephoneNumbers:
+            numbers.map(function(number) {
+              return {telephoneNumber: number};
+            })
         }
       };
-      helper.nock().post("/accounts/FakeAccountId/disconnects", helper.buildXml(data)).reply(200);
-      Disconnect.create(helper.createClient(), "test", ["111", "222"], done);
+      helper.nock().post("/accounts/FakeAccountId/removeImportedTnOrders", helper.buildXml(data)).reply(200, helper.xml.removeImportedTnOrderResponse, {"Content-Type": "application/xml"});
+      RemoveImportedTnOrder.create(helper.createClient(), "test", numbers, (err, item) => {
+        if(err){
+          return done(err);
+        }
+        item.id.should.eql("7527b3fc-3f72-4d0a-acae-ccc7e77857ed");
+        done();
+      });
     });
     it("should disconnect numbers (with default client)", function(done){
+      var numbers = ["9199918388", "4158714245", "4352154439", "4352154466"]
       var data = {
-        disconnectTelephoneNumberOrder: {
-          name: "test",
-          _nameXmlElement: "name",
-         disconnectTelephoneNumberOrderType: {
-           telephoneNumberList: [{telephoneNumber: "111"}, {telephoneNumber: "222"}]
-         }
+        removeImportedTnOrder:{
+          customerOrderId: "test",
+          _nameXmlElement: "CustomerOrderId",
+          telephoneNumbers:
+            numbers.map(function(number) {
+              return {telephoneNumber: number};
+            })
         }
       };
-      helper.nock().post("/accounts/FakeAccountId/disconnects", helper.buildXml(data)).reply(200);
-      Disconnect.create("test", ["111", "222"], done);
+      helper.nock().post("/accounts/FakeAccountId/removeImportedTnOrders", helper.buildXml(data)).reply(200, helper.xml.removeImportedTnOrderResponse, {"Content-Type": "application/xml"});
+      RemoveImportedTnOrder.create(helper.createClient(), "test", numbers, (err, item) => {
+        if(err){
+          return done(err);
+        }
+        item.id.should.eql("7527b3fc-3f72-4d0a-acae-ccc7e77857ed");
+        done();
+      });
     });
   });
+});
 });
