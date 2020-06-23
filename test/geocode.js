94a2461cf63a323c38b880c13651104c5194d9f5
Bryan Wu <bwu@bandwidth.com>
2020-06-23 18:17:47 -0400

    add error test for normal errors

--- test/geocode.js
+++ test/geocode.js
@@ -84,5 +84,21 @@ describe("Geocode", function(){
         done();
       });
     });
+
+    it("should report errors", function(done){
+      geoData = {
+        addressLine1: "Bad adrress line 1",
+        city: "City",
+        stateCode: "State",
+        zip: "ZipCode"
+      }
+      helper.nock().post("/accounts/FakeAccountId/geocodeRequest", helper.buildXml({requestAddress: geoData})).reply(400, {"Content-Type": "application/xml"});
+      Geocode.request(helper.createClient(), geoData, function(err, geocode){
+        if(err){
+          return done();
+        }
+        done(new Error('An error is estimated'));
+      });
+    });
   });
 });
