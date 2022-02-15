f7bb83f9537c8b8f3df37330a7b8a1c114b21496
Cameron Koegel <ckoegel@bandwidth.com>
2022-02-15 16:39:19 -0500

    log request

--- test/coveredRateCenter.js
+++ test/coveredRateCenter.js
@@ -7,15 +7,15 @@ lib.Client.globalOptions.userName = process.env.BW_USERNAME;
 lib.Client.globalOptions.password = process.env.BW_PASSWORD;
 
 describe("coveredRateCenter", function(){
-    before(function(){
-        lib.Client.globalOptions.apiEndPoint = "https://2da51ec2ee877f8091c24379fcb2091b.m.pipedream.net";
-        nock.disableNetConnect();
-    });
-    after(function(){
-        lib.Client.globalOptions.apiEndPoint = "https://dashboard.bandwidth.com/api";
-        nock.cleanAll();
-        nock.enableNetConnect();
-    });
+    // before(function(){
+    //     lib.Client.globalOptions.apiEndPoint = "https://2da51ec2ee877f8091c24379fcb2091b.m.pipedream.net";
+    //     nock.disableNetConnect();
+    // });
+    // after(function(){
+    //     lib.Client.globalOptions.apiEndPoint = "https://dashboard.bandwidth.com/api";
+    //     nock.cleanAll();
+    //     nock.enableNetConnect();
+    // });
     describe("#list", function(){
       it("should return list of coveredRateCenters", function(done){
         lib.CoveredRateCenter.list({zip: 27606, page: 1, size: 500}, function(err, list){
